6f433d6632714c9e537d067630a25ea8a8c9cd6a
Thomas W. Broadwater <thomaswbroadwater@gmail.com>
2022-06-07 09:40:50 -0400

    Major function changes
    Comment out SCRAPE() -- it was just a bad version of TEST() with a different name.
    
    Flush out TEST() -- now returns a list of variables for use in a tibble.
    
    Add experimenting lines -- short for() loop to gather and organize the first 100 entries of the Insciptiones Graecae.

--- InitialExploration/ScrapingPackhum/Scraper2.R
+++ InitialExploration/ScrapingPackhum/Scraper2.R
@@ -37,7 +37,7 @@ print(text)
 ### SCRAPBOOK ------------------------------------------------------------------
 
 # testing bad selector paths ---------------------------------------------------
-link <- 'https://inscriptions.packhum.org/text/1825'
+link <- 'https://inscriptions.packhum.org/text/0'
 page <- read_html(link)
 
 #textpage > div.hdr2 > span.fullref > a
@@ -48,38 +48,77 @@ print(PHIno)
 
 
 
-SCRAPE <- function() {
-  #for (i in c(1754:1764)) {
-  for (i in c(1:10)) {
-    # testing cleaning
-    #link <- 'https://inscriptions.packhum.org/text/1825'
-    link <- paste('https://inscriptions.packhum.org/text/', i, sep="")
-    page <- read_html(link)
-    
-    #textpage > div.text > div.greek.text-nowrap.dblclk > table
-    text <- page %>% html_nodes('div.greek.text-nowrap.dblclk') %>% html_text()
-    text <- gsub("[\\d\\[\\]]", "", text, perl=T)
-    text <- gsub("(\\-\\n{1,})", "", text, perl=T)
-    text <- gsub("\\n{1,}", "", text, perl=T)
-    text <- gsub("\\s{1,}", " ", text, perl=T)
-    
-    print(link)
-    print(text)
-  }
-}
+# SCRAPE <- function() {
+#   #for (i in c(1754:1764)) {
+#   for (i in c(1:10)) {
+#     # testing cleaning
+#     #link <- 'https://inscriptions.packhum.org/text/1825'
+#     link <- paste('https://inscriptions.packhum.org/text/', i, sep="")
+#     page <- read_html(link)
+#     
+#     #textpage > div.text > div.greek.text-nowrap.dblclk > table
+#     text <- page %>% html_nodes('div.greek.text-nowrap.dblclk') %>% html_text()
+#     text <- gsub("[\\d\\[\\]]", "", text, perl=T)
+#     text <- gsub("(\\-\\n{1,})", "", text, perl=T)
+#     text <- gsub("\\n{1,}", "", text, perl=T)
+#     text <- gsub("\\s{1,}", " ", text, perl=T)
+#     
+#     print(link)
+#     print(text)
+#   }
+# }
 
-TEST <- function(PHIno = 10) {
+TEST <- function(PHIno = 20) {
   link <- paste('https://inscriptions.packhum.org/text/', PHIno, sep="")
   page <- read_html(link)
   
+  ### Text
   #textpage > div.text > div.greek.text-nowrap.dblclk > table
   text <- page %>% html_nodes('div.greek.text-nowrap.dblclk') %>% html_text()
   text <- gsub("[\\d\\[\\]]", "", text, perl=T)
   text <- gsub("(\\-\\n{1,})", "", text, perl=T)
-  text <- gsub("\\n{1,}", "", text, perl=T)
+  text <- gsub("\\n{1,}", " ", text, perl=T)
   text <- gsub("\\s{1,}", " ", text, perl=T)
+  # ̣COMBINING DOT BELOW Unicode: U+0323, UTF-8: CC A3
+  text <- gsub("̣", "", text, perl=T)
+  text <- gsub("#", "", text, perl=T)
+  # ℎ PLANCK CONSTANT Unicode: U+210E, UTF-8: E2 84 8E
+  text <- gsub("ℎ", "h", text, perl=T)
   
-  print(link)
-  print(text)
+  ### IGBook
+  #textpage > div.hdr2 > span.fullref > a
+  IGbook <- page %>% html_nodes("span.fullref > a") %>% html_text()
+  IGbook <- gsub("\\n", "", IGbook)
   
+  ### IGNo
+  IGno <- page %>% html_nodes("span.fullref > span") %>% html_text()
+  IGno <- gsub("\\n", "", IGno)
+  
+  # Atm keeping for debugging
+  # print(link)
+  # print(text)
+  # print(IGbook)
+  # print(IGno)
+  # print(PHIno)
+  
+  # Make entry to be returned
+  entry <- c(IGbook, IGno, PHIno, text, link)
+  return(as.list(entry))
+  
+  # Atm keeping for debugging
+  # rm(link)
+  # rm(text)
+  # rm(IGbook)
+  # rm(IGno)
+  # rm(PHIno)
+}
+
+# df[row, column]
+test_df <- tibble(Book = NA, No = NA, Ref = NA, Text = NA, Link = NA)
+test_df[1,] <- TEST(1)
+test_df[2,] <- TEST(2)
+test_df[3,] <- TEST(3)
+
+for (i in c(1:100)) {
+  test_df[i,] <- TEST(i)
 }
